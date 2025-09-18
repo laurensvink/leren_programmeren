15f2c787c9b22a46d06f6ac690c312c86ac69054
laurensvink <99087293@mydavinci.nl>
2025-09-18 11:53:03 +0200

    Update feestlunch.py

--- module_1/deel_1_omgaan_met_gegevens/feestlunch.py
+++ module_1/deel_1_omgaan_met_gegevens/feestlunch.py
@@ -1,4 +1,3 @@
 totaal = (17 * 0.39) + (2 * 2.78) - (3 * 0.50)
-
-# Afronden op 2 decimalen en tonen
+print(f"De feestlunch kost je bij de bakker {totaal} voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
 print("Te betalen bedrag: €{:.2f}".format(totaal))
