fc50fdb2a9f9c1a4b19e060ece7fa2ef6d0777bf
laurensvink <99087293@mydavinci.nl>
2025-09-18 11:53:06 +0200

    Update speelhal.py

---
+++ module_1/deel_1_omgaan_met_gegevens/speelhal.py
@@ -0,0 +1,4 @@
+totaal = (5*7.50) + (9*0.37*5) 
+delen = totaal/2
+print(f"Dit geweldige dagje-uit met 5 mensen in de Speelhal met 45 minuten VR kost je maar {totaal} euro per persoon voor 2 mensen") 
+print(delen)   
