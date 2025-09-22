7e5cb76d0514d3820453fe371aacba74bff94a70
laurensvink <99087293@mydavinci.nl>
2025-09-22 09:05:02 +0200

    Create idk.py

---
+++ module_1/deel_1_omgaan_met_gegevens/idk.py
@@ -0,0 +1,9 @@
+leeftijd = int(input("hoe oud ben je?\n"))
+if leeftijd == 16:
+    print("kaasschijf123")
+elif leeftijd >= 18:
+        print("je mag stemmen")
+elif leeftijd <18 and leeftijd > 12:
+      print("je bent een tiener")  
+else:
+      print("my guy je bent net geboren wat doe je op python")            
