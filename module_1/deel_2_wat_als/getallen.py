0e6c8136db38342daf9babe8752fe265d5cc2e69
laurensvink <99087293@mydavinci.nl>
2025-09-25 11:22:27 +0200

    Update getallen.py

---
+++ module_1/deel_2_wat_als/getallen.py
@@ -0,0 +1,9 @@
+getal_1 = int(input("geef een getal"))
+getal_2 = int(input("geef nog een getal"))
+
+if getal_1 == getal_2 :
+    print("de getallen zijn gelijk")
+elif getal_1 > getal_2 :
+    print("getal 1 is groter dan getal 2")
+else :
+ print("getal 2 is groter dan getal 1")
