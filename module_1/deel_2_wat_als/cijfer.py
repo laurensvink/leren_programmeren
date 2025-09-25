3fa28c89214fc72b57cfd08eab86b48429935040
laurensvink <99087293@mydavinci.nl>
2025-09-25 11:22:32 +0200

    Create cijfer.py

---
+++ module_1/deel_2_wat_als/cijfer.py
@@ -0,0 +1,22 @@
+cijfer = int(input("wat is je cijfer?"))
+
+if cijfer == 10 :
+    print(f"uitmuntend je cijfer is een {cijfer}")
+if cijfer == 9 :
+    print(f"zeer goed je cijfer is een {cijfer}") 
+if cijfer == 8 :
+    print(f"goed je cijfer is een {cijfer}") 
+if cijfer == 7 : 
+    print(f"ruim voldoende je cijfer is een {cijfer}")      
+if cijfer == 6:
+    print(f"voldoende je cijfer is een {cijfer}") 
+if cijfer == 5 :
+    print(f"bijna voldoende jammer je cijfer is een {cijfer}") 
+if cijfer == 4 :
+    print(f"onvoldoende jammer je cijfer is een {cijfer}")
+if cijfer == 3 :
+    print(f"gering jammer je cijfer is een {cijfer}")
+if cijfer == 2 :
+    print(f"slecht jammer je cijfer is een {cijfer}")  
+if cijfer == 1 :
+    print(f"zeer slecht jammer je cijfer is een {cijfer}")
