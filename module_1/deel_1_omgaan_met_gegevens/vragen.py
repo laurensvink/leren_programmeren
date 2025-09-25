a437180277800e351214159548e20ed6cb95a8b5
laurensvink <99087293@mydavinci.nl>
2025-09-25 10:59:50 +0200

    Create vragen.py

---
+++ module_1/deel_1_omgaan_met_gegevens/vragen.py
@@ -0,0 +1,6 @@
+naam = input("wat is je naam? ")
+leeftijd = input(f"Hallo {naam} hoe oud ben je? ")
+fav_eten = input(f"en als {leeftijd} jarige eet je het liefst? ")
+fav_drinken = input(f"en wat is je favorite drinken bij {fav_eten}? ")
+
+print(f"De {leeftijd} jarige {naam} drinkt het liefst {fav_drinken} bij {fav_eten}")
