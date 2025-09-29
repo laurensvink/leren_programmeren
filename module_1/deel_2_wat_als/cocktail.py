8bb353adee9f81cca6930a3e7fd6426b5aa94496
laurensvink <99087293@mydavinci.nl>
2025-09-29 10:13:02 +0200

    Update cocktail.py

--- module_1/deel_2_wat_als/cocktail.py
+++ module_1/deel_2_wat_als/cocktail.py
@@ -1,1 +1,5 @@
 print("ik ga je cocktail raden door middel van een aantal vragen")
+print("beantwoord de vragen met ja of nee")
+
+antwoord_1 = input("Zit er rood in het drankje?")
+if antwoord_1 == "ja" :
