2b264817ce0da7083d3fb8ddcf72b0efd7242f0d
laurensvink <99087293@mydavinci.nl>
2025-09-29 10:13:10 +0200

    Update pizza.py

--- module_1/deel_1_omgaan_met_gegevens/pizza.py
+++ module_1/deel_1_omgaan_met_gegevens/pizza.py
@@ -4,7 +4,7 @@ aantal_big_pizza = int(input("hoeveel big pizza's wilt u bestellen?"))
 
 prijs_small_pizza = 5
 prijs_medium_pizza = 10
-prijs_big_pizza = 15 
+prijs_big_pizza = 20 
 
 gekochte_small_pizza = aantal_small_pizza*prijs_small_pizza
 gekochte_medium_pizza = aantal_medium_pizza*prijs_medium_pizza
@@ -15,6 +15,6 @@ totaal = gekochte_big_pizza+gekochte_medium_pizza+gekochte_small_pizza
 print("************** KASSA BON **************")
 print(f"Pizza's Small:              {aantal_small_pizza} x 5 = €{gekochte_small_pizza}")
 print(f"Pizza's Medium:            {aantal_medium_pizza} x 10 = €{gekochte_medium_pizza}")
-print(f"Pizza's Big:               {aantal_big_pizza} x 15 = €{gekochte_big_pizza}")
+print(f"Pizza's Big:               {aantal_big_pizza} x 20 = €{gekochte_big_pizza}")
 print("--------------------------------------")
 print(f"Te betalen                         €{totaal}")
