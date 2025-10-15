9cbbdbb13d87b09d57e7a0823689a53b9dd50c95
laurensvink <99087293@mydavinci.nl>
2025-10-15 09:43:11 +0200

    Update pizza.py

--- module_1/deel_1_omgaan_met_gegevens/pizza.py
+++ module_1/deel_1_omgaan_met_gegevens/pizza.py
@@ -1,29 +1,37 @@
-try :
-    aantal_small_pizza = int(input("hoeveel small pizza's wilt u bestellen?"))
-except : 
-    print("dat is geen heel getal!")
-try :
-    aantal_medium_pizza = int(input("hoeveel medium pizza's wilt u bestellen?"))
-except : 
-    print("dat is geen heel getal!")
-try :
-    aantal_big_pizza = int(input("hoeveel big pizza's wilt u bestellen?"))
-except :
-    print("dat is geen heel getal!")
+try:
+    aantal_small_pizza = int(input("Hoeveel small pizza's wilt u bestellen? "))
+except:
+    print("Dat is geen heel getal! Er worden 0 small pizza's gerekend.")
+    aantal_small_pizza = 0
+
+try:
+    aantal_medium_pizza = int(input("Hoeveel medium pizza's wilt u bestellen? "))
+except:
+    print("Dat is geen heel getal! Er worden 0 medium pizza's gerekend.")
+    aantal_medium_pizza = 0
+
+try:
+    aantal_big_pizza = int(input("Hoeveel big pizza's wilt u bestellen? "))
+except:
+    print("Dat is geen heel getal! Er worden 0 big pizza's gerekend.")
+    aantal_big_pizza = 0
+
 
 prijs_small_pizza = 5
 prijs_medium_pizza = 10
-prijs_big_pizza = 20 
+prijs_big_pizza = 20
 
-gekochte_small_pizza = aantal_small_pizza*prijs_small_pizza
-gekochte_medium_pizza = aantal_medium_pizza*prijs_medium_pizza
-gekochte_big_pizza = aantal_big_pizza*prijs_big_pizza
-   
-totaal = gekochte_big_pizza+gekochte_medium_pizza+gekochte_small_pizza
+gekochte_small_pizza = aantal_small_pizza * prijs_small_pizza
+gekochte_medium_pizza = aantal_medium_pizza * prijs_medium_pizza
+gekochte_big_pizza = aantal_big_pizza * prijs_big_pizza
+totaal = gekochte_small_pizza + gekochte_medium_pizza + gekochte_big_pizza
 
-print("************** KASSA BON **************")
-print(f"Pizza's Small:              {aantal_small_pizza} x 5 = €{gekochte_small_pizza}")
-print(f"Pizza's Medium:            {aantal_medium_pizza} x 10 = €{gekochte_medium_pizza}")
-print(f"Pizza's Big:               {aantal_big_pizza} x 20 = €{gekochte_big_pizza}")
-print("--------------------------------------")
-print(f"Te betalen                         €{totaal}")
+print("\n************** KASSA BON **************")
+if aantal_small_pizza != 0:
+    print(f"Pizza's Small:    {aantal_small_pizza} x €5  = €{gekochte_small_pizza}")
+if aantal_medium_pizza != 0:
+    print(f"Pizza's Medium:   {aantal_medium_pizza} x €10 = €{gekochte_medium_pizza}")
+if aantal_big_pizza != 0:
+    print(f"Pizza's Big:      {aantal_big_pizza} x €20 = €{gekochte_big_pizza}")
+print("----------------------------------------")
+print(f"TOTAAL TE BETALEN:              €{totaal}")
