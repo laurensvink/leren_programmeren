6ca92eab6b78cf0c76f79a4a279150de42c03f5a
laurensvink <99087293@mydavinci.nl>
2025-11-06 09:51:38 +0100

    ee

--- module_1/deel_2_wat_als/studieadvies.py
+++ module_1/deel_2_wat_als/studieadvies.py
@@ -1,28 +1,41 @@
 from studieadviestext import *
-score = 0 
 
 print(STUDIEDOKTER_TITEL)
 input(AANTAL_WEKEN_VRAAG)
-print(COMPETENTIE_STELLING_1)
-score += int(input(OPTIES))
-print(COMPETENTIE_STELLING_2)
-score += int(input(OPTIES))
-print(COMPETENTIE_STELLING_3)
-score += int(input(OPTIES))
-print(COMPETENTIE_STELLING_4)
-score += int(input(OPTIES))
-print(COMPETENTIE_STELLING_5)
-score += int(input(OPTIES))
-print(COMPETENTIE_STELLING_6)
-score += int(input(OPTIES))
-print(COMPETENTIE_STELLING_7)
-score += int(input(OPTIES))
-gemiddelde = score/7
+
+stellingen = [
+    COMPETENTIE_STELLING_1,
+    COMPETENTIE_STELLING_2,
+    COMPETENTIE_STELLING_3,
+    COMPETENTIE_STELLING_4,
+    COMPETENTIE_STELLING_5,
+    COMPETENTIE_STELLING_6,
+    COMPETENTIE_STELLING_7
+]
+
+score = 0
+aantal_vragen = 0
+altijd_vaak = 0
+altijd_vaak_regelmatig = 0
+
+for stelling in stellingen:
+    print(stelling)
+    antwoord = int(input(OPTIES))
+    score += antwoord
+    aantal_vragen += 1
+
+    if antwoord <= 1:
+        altijd_vaak += 1
+    if antwoord <= 2:
+        altijd_vaak_regelmatig += 1
+
+gemiddelde = score / aantal_vragen
 
 print(COMPETENTIE_ADVIES_TITEL)
-if gemiddelde <= 2 :
+
+if gemiddelde <= 2 or altijd_vaak > aantal_vragen / 2:
     print(COMPETENTIE_ADVIES_ZORGELIJK)
-if gemiddelde <= 3 :
+elif gemiddelde <= 3 or altijd_vaak_regelmatig > aantal_vragen / 2:
     print(COMPETENTIE_ADVIES_TWIJFELACHTIG)
-else :
+else:
     print(COMPETENTIE_ADVIES_GERUSTSTELLEND)
--- module_1/deel_3_fouten_en_uitzonderingen/ice_bugs.py
+++ module_1/deel_3_fouten_en_uitzonderingen/ice_bugs.py
@@ -6,4 +6,4 @@ MEDIUM_PRICE = 2.10
 small_amount = int(input('Hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(SMALL_PRICE))))
 medium_amount = int(input('En hoeveel ijsjes van {} wil je bestellen? '.format(convertToEuroText(MEDIUM_PRICE))))
 total_price = (small_amount * SMALL_PRICE) + (medium_amount * MEDIUM_PRICE)
-print('Dat is dan {} totaal'.format(convertToEuroText(total_price)))
+print('Dat is dan {} totaal'.format(convertToEuroText(total_price))) 
